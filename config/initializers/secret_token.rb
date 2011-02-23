5395bd7c1de2b7eb5063768f46b8ff8fe96dda4b
ysta <ysta@kk.bsnet.se>
2011-02-24 00:18:47 +0100

    first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kk::Application.config.secret_token = '7132ff6500e36d99b676b789be3ce1214cac022e467bf19923d4533fcd944a144b78496774df66af973b20b2665a99a1de3a5c4b4e82778b7e2327482ca9d98b'
