1e9e0c9ba4cc977d0630054c8f5192a11aebcb07
ysta <ysta@kk.bsnet.se>
2011-11-29 20:51:45 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110402141646) do
+ActiveRecord::Schema.define(:version => 20110224233255) do
 
   create_table "kinkynights", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.text "text", :null => false
   end
 
   create_table "notices", :force => true do |t|
